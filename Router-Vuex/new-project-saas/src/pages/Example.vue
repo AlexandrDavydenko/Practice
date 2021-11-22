468ccfc8e427b3d1f082a6b63b6b9ea56ebdc8d5
AlexandrDavydenko <76471101+AlexandrDavydenko@users.noreply.github.com>
2021-11-23 01:04:11 +0600

    Add files via upload

---
+++ Router-Vuex/new-project-saas/src/pages/Example.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="wrapper-content wrapper-content--fixed">
+      <section>
+        <div class="container">
+            <h1>Example page</h1>
+            <!-- <p>{{ getMessage }}</p>
+            <input type="text" v-model="message">
+            <button class="btn btnPrimary" @click="setMessage">Change message</button>
+   <br>
+   <br>      -->
+        </div>
+      </section>
+    </div>
+</template>
